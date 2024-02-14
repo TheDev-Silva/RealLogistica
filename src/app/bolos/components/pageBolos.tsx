926fec88c11a98443be8a0b7b701ecbed2741456
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-14 06:05:14 -0300

    feat: melhorias nos layout das categorias

--- src/app/bolos/components/pageBolos.tsx
+++ src/app/bolos/components/pageBolos.tsx
@@ -6,7 +6,7 @@ export default function PageBolos() {
             <div className="md:w-[100%] md:py-4 py-10 md:flex md:justify-between items-center justify-center rounded-lg md:h-[300px] h-auto  bg-gradient-to-tr from-[#ff6600] to-[#663300]">
                 <div className="w-full flex items-center justify-center">
                     <h1 style={{ fontFamily: "Bebas Neue" }} className="md:text-[160px] text-[100px] absolute font-bold">Bolos</h1>
-                    <p style={{ fontFamily: "Blooming Elegant Monoline", letterSpacing: '2px', color: "white" }} className="md:text-[100px] text-[40px]  relative md:bottom-[40px] bottom-[20px]">confeitados</p>
+                    <p style={{ fontFamily: "Arizona", letterSpacing: '2px', color: "white" }} className="md:text-[80px] text-[50px] relative md:bottom-[0px] bottom-[0px]drop-shadow-lg">confeitados</p>
                 </div>
                 <div className="max-w-[100%] min-w-[50%] max-h-[100%] md:flex items-center justify-center hidden">
                     <Image src="/banner/banner-bolo-confeitados.png" alt="bolo" width={500} height={500} className="w-[450px]"/>
--- src/app/trufa/components/pageTrufa.tsx
+++ src/app/trufa/components/pageTrufa.tsx
@@ -5,8 +5,8 @@ export default function PageTrufa() {
         <div className="p-2">
             <div className="md:w-[100%] md:py-4 py-10 md:flex md:justify-between items-center justify-center rounded-lg md:h-[300px] h-auto bg-gradient-to-tr from-[#ff6600] to-[#663300]">
                 <div className="w-[100%] h-[100%] flex items-center justify-center text-center">
-                    <h1 style={{ fontFamily: "Bebas Neue" }} className="md:text-[160px] text-[100px] absolute font-bold">Trufas</h1>
-                    <p style={{ fontFamily: "Blooming Elegant Monoline", letterSpacing: '2px', color: "white" }} className="md:text-[100px] text-[40px]  relative md:bottom-[40px] bottom-[20px]">recheados</p>
+                    <h1 style={{ fontFamily: "Bebas Neue" }} className="md:text-[160px] text-[100px] absolute font-bold ">Trufas</h1>
+                    <p style={{ fontFamily: "Arizona", letterSpacing: '2px', color: "white"}} className="md:text-[80px] text-[50px] relative md:bottom-[0px] bottom-[0px]drop-shadow-lg">recheados</p>
                 </div>
                 <div className="max-w-[100%] min-w-[50%] max-h-[100%] md:flex items-center justify-center hidden">
                     <Image src="/banner/banner-trufa-recheadas.png" alt="trufa" width={500} height={500} className="w-[300px]"/>
