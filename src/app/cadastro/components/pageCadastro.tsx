ee064528598d170e2c35f9f7b5c8c1ff2916e37f
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-17 17:44:00 -0300

    feat: algumas alterações.

--- src/app/cadastro/components/pageCadastro.tsx
+++ src/app/cadastro/components/pageCadastro.tsx
@@ -20,18 +20,18 @@ export default function PageCadastro() {
   }
 
   return (
-    <div className="pt-[160px] p-2 bg-slate-100 md:w-full flex flex-col">
+    <div className=" p-2 bg-transparent md:w-full flex flex-col">
       <div className="flex items-end justify-end pb-2">
         <Link href="/">
           <Button>Voltar</Button>
         </Link>
       </div>
-      <div className="gap-2 min-h-[400px] md:flex flex-grow p-5 md:p-0 bg-blend-saturation bg-slate-100">
+      <div className="gap-2 min-h-[400px] md:flex flex-grow p-5 md:p-0 bg-blend-saturation bg-transparent">
         <div className="w-full rounded-sm p-5 flex flex-col items-center justify-between">
           <h1 className="text-[120px] font-bold " style={{fontFamily: 'Bebas Neue', lineHeight: '100px'}}>Ainda Não tem cadastro? </h1><p style={{fontFamily: 'Arizona', fontSize: '60px', color: '#ff6600', textShadow: "1px 0px 2px #000", letterSpacing: '2px'}}>se increva já</p>
             <Button className="uppercase w-[400px] h-14">inscrever</Button>
         </div>
-        <div className="w-full p-5 md:p-0 flex flex-col items-center justify-center bg-[#ff6600] rounded-lg gap-4">
+        <div className="w-full p-5 md:p-0 flex flex-col items-center justify-center backdrop-blur-sm bg-opacity-30 bg-[#ff6600] rounded-lg gap-4">
           <div className="flex gap-4">
             
             <Button className="min-w-[200px]">Fazer Login</Button>
