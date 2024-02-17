6ba5a901ba46868cd2ecc744c5a4153fa9ebc4a1
Germano Silva <silvadeveloper2024@gmail.com>
2024-02-17 17:45:31 -0300

    feat: importei a PageCadastro/alteration

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { signIn, useSession } from "next-auth/react"
 import Link from "next/link"
+import PageCadastro from "../cadastro/components/pageCadastro"
 
 export default function Home() {
 
@@ -13,16 +14,8 @@ export default function Home() {
   }
 
   return (
-    <div className="w-full h-[100vh] pt-[160px] flex items-center justify-center">
-      {status === 'authenticated' ? (
-        <div className="font-bold flex flex-col items-center justify-center">
-          <h1 className="uppercase text-[30px]">Bem-vindo!</h1> <p className="font-semibold">{data.user?.name}</p>
-        </div>
-      ) : (
-        <div className="gap-2">
-          Você não se encontra logado clique <a onClick={Logar} className="w-[120px] cursor-pointer underline font-semibold">AQUI</a>  para ser redirecionado ou aqui para se <Link href="/cadastro" className="cursor-pointer underline font-semibold uppercase">inscrever</Link>
-        </div>
-      )}
+    <div className="w-full h-[100vh] pt-[160px] flex items-center justify-center" style={{backgroundImage: 'url(/images/carousel/plano-de-fundo.jpg)'}}>
+      <PageCadastro/>
     </div>
   )
 }
